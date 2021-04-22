1788e7687e5c040686f9fb02731444174e0d1345
Zak Stamps <theoneandonlyzako@gmail.com>
2021-04-22 15:48:45 -0700

    added language sorter

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,71 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+// to be executed upon a form submission browser event.
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    //  Get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    }else {
+        alert("Please enter a Github uername");
+    }
+    console.log(event);   
+};
+
+var buttonClickHandler = function (event) {
+    // gets the language attribute from the button clicked
+    var language = event.target.getAttribute("data-language");
+    
+    if (language) {
+        getFeaturedRepos(language);
+
+        // Clear Old Content
+        repoContainerEl.textContent = "";
+    }
+}
+
+var getUserRepos = function(user) {
+    // format the github api url
+var apiUrl = "https://api.github.com/users/" + user + "/repos";
+// Variables to store a referebce to the form
+
+// make a request to the url
+fetch(apiUrl)
+    .then(function(response) {
+    if (response.ok) {
+        response.json().then(function(data) {
+            displayRepos(data, user);
+        });
+    }else {
+        alert("Error: " + response.statusText); 
+    }
+})
+.catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method 
+    alert("Unable to connect to GitHub");
+});
+};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+            // console.log(response);
+        }else {
+            alert('Error: Github User Not Found');
+        }
+    });
+};
 
 var displayRepos = function(repos, searchTerm) {
     // Check if api returned any repos
@@ -52,41 +117,7 @@ repoEl.appendChild(statusEl);
     }
 }
 
-// to be executed upon a form submission browser event.
-var formSubmitHandler = function(event) {
-    event.preventDefault();
-    //  Get value from input element
-    var username = nameInputEl.value.trim();
-
-    if (username) {
-        getUserRepos(username);
-        nameInputEl.value = "";
-    }else {
-        alert("Please enter a Github uername");
-    }
-    console.log(event);   
-};
-
-var getUserRepos = function(user) {
-    // format the github api url
-var apiUrl = "https://api.github.com/users/" + user + "/repos";
-// Variables to store a referebce to the form
-
-// make a request to the url
-fetch(apiUrl)
-    .then(function(response) {
-    if (response.ok) {
-        response.json().then(function(data) {
-            displayRepos(data, user);
-        });
-    }else {
-        alert("Error: " + response.statusText); 
-    }
-})
-.catch(function(error) {
-    // Notice this `.catch()` getting chained onto the end of the `.then()` method 
-    alert("Unable to connect to GitHub");
-});
-};
-
+// Event listeners to form and button container
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
